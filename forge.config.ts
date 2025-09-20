52c8abf2e333fe7fb794ba41ec37c8a46bd9524c
Lachezar Tsochev <ltsochev@live.com>
2025-09-20 22:16:42 +0300

    fixing build targets

--- forge.config.ts
+++ forge.config.ts
@@ -1,5 +1,5 @@
 import type { ForgeConfig } from "@electron-forge/shared-types";
-import { MakerSquirrel } from "@electron-forge/maker-squirrel";
+// import { MakerSquirrel } from "@electron-forge/maker-squirrel";
 import { MakerZIP } from "@electron-forge/maker-zip";
 import { MakerDeb } from "@electron-forge/maker-deb";
 import { MakerRpm } from "@electron-forge/maker-rpm";
@@ -13,8 +13,8 @@ const config: ForgeConfig = {
   },
   rebuildConfig: {},
   makers: [
-    new MakerSquirrel({}),
-    new MakerZIP({}, ["darwin"]),
+    // new MakerSquirrel({}),
+    new MakerZIP({}, ["win32", "darwin"]),
     new MakerRpm({}),
     new MakerDeb({}),
   ],
