3c35eb868f52eb5b86e1c3b98a25dbe21ebf506a
Lachezar Tsochev <ltsochev@live.com>
2025-09-20 21:42:04 +0300

    build output fix

--- vite.web.config.ts
+++ vite.web.config.ts
@@ -5,6 +5,10 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   root: "src/web",
+  build: {
+    outDir: path.resolve(__dirname, ".vite/renderer/web"),
+    emptyOutDir: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
